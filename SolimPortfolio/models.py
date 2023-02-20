d3aff5c14267219d61b039860af57057a5fa6bad
LAOKPEZI Solim <lao2fran@gmail.com>
2023-02-20 01:09:05 +0100

    first commit

---
+++ SolimPortfolio/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    desc = models.TextField()
+    image = models.ImageField(upload_to="articles")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
